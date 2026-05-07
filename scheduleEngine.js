51dffb5797308f7080fb0faadf7be96698db871a
Nayon Roy <157906187+NayonRoy437@users.noreply.github.com>
2026-05-07 21:20:43 +0600

    Update scheduleEngine.js

--- scheduleEngine.js
+++ scheduleEngine.js
@@ -1,32 +1,71 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { db }
+from "./firebase.js";
+
+import {
+collection,
+getDocs
+}
+
+from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
 
 export async function getCurrentAndNextClass(){
 
     let now = new Date();
+
     let hour = now.getHours();
 
-    let snapshot = await getDocs(collection(db, "classes"));
+    let snapshot =
+    await getDocs(collection(db,"classes"));
 
     let classes = [];
 
     snapshot.forEach(doc => {
-        classes.push(doc.data());
+
+        classes.push({
+
+            id: doc.id,
+
+            ...doc.data()
+
+        });
+
     });
 
+    // SORT BY START TIME
+    classes.sort(
+        (a,b) => a.startHour - b.startHour
+    );
+
     let current = null;
+
     let next = null;
 
     for(let c of classes){
 
-        if(hour >= c.startHour && hour < c.endHour){
+        // CURRENT CLASS
+        if(
+            hour >= c.startHour &&
+            hour < c.endHour
+        ){
+
             current = c;
         }
 
-        if(c.startHour > hour && !next){
+        // NEXT CLASS
+        if(
+            c.startHour > hour &&
+            !next
+        ){
+
             next = c;
         }
     }
 
-    return { current, next };
+    return {
+
+        current,
+        next
+
+    };
 }
