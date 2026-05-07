d4d7882e2725c315ee24d7092b856c8380e0e6e8
Nayon Roy <157906187+NayonRoy437@users.noreply.github.com>
2026-05-07 21:26:30 +0600

    Update conflict.js

--- conflict.js
+++ conflict.js
@@ -1,18 +1,80 @@
-export function hasConflict(classes,newClass){
+let result =
+hasConflict(allClasses,newClass);
+
+if(result.conflict){
+
+    message.innerHTML =
+    result.message;
+
+    return;
+}
+export function hasConflict(
+classes,
+newClass
+){
+
+    // INVALID TIME
+    if(
+        newClass.startHour >=
+        newClass.endHour
+    ){
+
+        return {
+            conflict:true,
+            message:"❌ Invalid Time"
+        };
+    }
 
     for(let c of classes){
 
-        let sameRoom =
-        c.room === newClass.room;
-
+        // TIME OVERLAP
         let overlap =
+
         newClass.startHour < c.endHour &&
+
         newClass.endHour > c.startHour;
 
-        if(sameRoom && overlap){
-            return true;
+
+        // ROOM CONFLICT
+        let sameRoom =
+
+        c.room === newClass.room;
+
+
+        // TEACHER CONFLICT
+        let sameTeacher =
+
+        c.teacher === newClass.teacher;
+
+
+        if(overlap && sameRoom){
+
+            return {
+
+                conflict:true,
+
+                message:
+                "❌ Room Conflict"
+
+            };
+        }
+
+        if(overlap && sameTeacher){
+
+            return {
+
+                conflict:true,
+
+                message:
+                "❌ Teacher Conflict"
+
+            };
         }
     }
 
-    return false;
+    return {
+
+        conflict:false
+
+    };
 }
