eafe226e20e7ebbae271e55ac0fc65d39293a1ee
Nayon Roy <157906187+NayonRoy437@users.noreply.github.com>
2026-05-08 11:44:00 +0600

    Update scheduleEngine.js

--- scheduleEngine.js
+++ scheduleEngine.js
@@ -3,7 +3,9 @@ import { db } from "./firebase.js";
 import {
 collection,
 getDocs
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+}
+
+from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 
 export async function getCurrentAndNextClass(){
@@ -14,7 +16,8 @@ export async function getCurrentAndNextClass(){
 
     let minute = now.getMinutes();
 
-    let currentTime = hour + minute / 60;
+    let currentTime =
+    hour + minute / 60;
 
     let snapshot =
     await getDocs(collection(db,"classes"));
@@ -24,39 +27,60 @@ export async function getCurrentAndNextClass(){
     snapshot.forEach(doc => {
 
         classes.push({
+
             id: doc.id,
+
             ...doc.data()
+
         });
 
     });
 
-    // SORT BY START TIME
-    classes.sort((a,b)=>
-        a.startHour - b.startHour
+    // SORT
+    classes.sort(
+
+        (a,b)=>
+
+        Number(a.startHour) -
+
+        Number(b.startHour)
+
     );
 
     let current = null;
+
     let next = null;
 
     let minGap = Infinity;
 
+
     for(let c of classes){
 
-        let start = c.startHour;
-        let end = c.endHour;
+        let start =
+        Number(c.startHour);
 
-        // CURRENT CLASS (more accurate)
+        let end =
+        Number(c.endHour);
+
+
+        // CURRENT CLASS
         if(
+
             currentTime >= start &&
+
             currentTime < end
+
         ){
+
             current = c;
         }
 
-        // NEXT CLASS (closest future class)
+
+        // NEXT CLASS
         if(start > currentTime){
 
-            let gap = start - currentTime;
+            let gap =
+            start - currentTime;
 
             if(gap < minGap){
 
@@ -68,7 +92,9 @@ export async function getCurrentAndNextClass(){
     }
 
     return {
+
         current,
         next
+
     };
 }
